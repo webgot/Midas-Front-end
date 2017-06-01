c56628400f877cd3ac58bbc14bb67ed5591e372f
webgot <webgot.js@gmail.com>
2017-06-01 16:04:41 +0900

    total commit

---
+++ js/main.js
@@ -0,0 +1,17 @@
+function ShowLoginBlock() {
+    $('.login-block').css('display', 'block');
+    $('.join-block').css('display', 'none');
+}
+function ShowJoinBlock() {
+    $('.login-block').css('display', 'none');
+    $('.join-block').css('display', 'block');
+}
+$(function () {
+    $('.toLogin').click(function (event) {
+        ShowLoginBlock();
+    });
+    $('.toJoin').click(function (event) {
+        ShowJoinBlock();
+    });
+    $('.ui.rating').rating();
+});
